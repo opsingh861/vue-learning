0a9fe252a565f34df25c4f90d1c986225e6a08df
Aditya Dhanraj <opsingh861@gmail.com>
2024-05-26 23:42:49 +0530

    Dynamic attributes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>// we need to add setup so that we can use the script tag in the single file component
-const msg = "hello world";
-const imgSrc = "https://vuejs.org/images/logo.png";
-const title = "Attribute binding";
-const height = 500;
-const width = 500;
+const imageInfo ={
+  src: 'https://vuejs.org/images/logo.png',
+  alt: 'Vue.js Logo',
+  width: '100',
+  height: '100'
+
+}
 </script>
 
 <template>
-  <h1>Attribute binding</h1>
-  <!-- <img :src="imgSrc" alt="title" height="500" width="500" /> if we just want to hard code the value we can just give the value directly -->
-  <!-- <img :src="imgSrc" alt="title" :height="height" :width="width" /> -->
-  <img v-bind:src="imgSrc" alt="title" v-bind:height="height" v-bind:width="width" />
+  <h1>Dynamic attributes</h1>
+  <img :="imageInfo" />
 </template>
