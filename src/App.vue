500eff0fa0cf68d2751ffedfa2acde479c5abb86
Aditya Dhanraj <opsingh861@gmail.com>
2024-05-26 23:39:37 +0530

    Attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
-<script setup> // we need to add setup so that we can use the script tag in the single file component
+<script setup>// we need to add setup so that we can use the script tag in the single file component
 const msg = "hello world";
+const imgSrc = "https://vuejs.org/images/logo.png";
+const title = "Attribute binding";
+const height = 500;
+const width = 500;
 </script>
 
 <template>
-  <h1>hello world</h1>
-  <h1>String interpolation: {{ msg }} </h1>
+  <h1>Attribute binding</h1>
+  <!-- <img :src="imgSrc" alt="title" height="500" width="500" /> if we just want to hard code the value we can just give the value directly -->
+  <!-- <img :src="imgSrc" alt="title" :height="height" :width="width" /> -->
+  <img v-bind:src="imgSrc" alt="title" v-bind:height="height" v-bind:width="width" />
 </template>
