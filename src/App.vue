8e81cd7e381051fdc0c8e3f82b3ebd2fd3d70175
Aditya Dhanraj <opsingh861@gmail.com>
2024-05-26 21:18:15 +0530

    text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup> // we need to add setup so that we can use the script tag in the single file component
+const msg = "hello world";
+</script>
+
 <template>
   <h1>hello world</h1>
+  <h1>String interpolation: {{ msg }} </h1>
 </template>
