2660781baeab3632ee22be7a279ee1a8043b9cea
Aditya Dhanraj <opsingh861@gmail.com>
2024-05-27 11:10:00 +0530

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>// we need to add setup so that we can use the script tag in the single file component
-const imageInfo ={
-  src: 'https://vuejs.org/images/logo.png',
-  alt: 'Vue.js Logo',
-  width: '100',
-  height: '100'
-
-}
+import MyComponent from './components/MyComponent.vue'
 </script>
 
 <template>
-  <h1>Dynamic attributes</h1>
-  <img :="imageInfo" />
+  <div>
+    <MyComponent />
+    <h1 class="global">This is global css from App component</h1>
+    <h1 class="local">This is local css from App component</h1>  <!-- this will not work because the css is local to MyComponent.vue -->
+  </div>
 </template>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="global">This is global CSS from MyComponent</h1>
+        <h1 class="local">This is local CSS from MyComponent</h1>
+        <h1 :class="$style['kabab-case']">This is modular css using kabab case</h1>
+        <h1 :class="$style.camleCase">This is modular css using camel case</h1>
+        <h1 :class="myStyle.custom">This is modular css using custom module name</h1>
+    </div>
+</template>
+
+<style>
+.global {
+    color: red;
+}
+</style>
+
+<style scoped>
+.local {
+    color: blue;
+}
+</style>
+
+<style module>
+.kabab-case {
+    color: green;
+}
+
+.camleCase {
+    color: yellow;
+}
+</style>
+
+<style module="myStyle">
+.custom {
+    color: purple;
+}
+</style>
